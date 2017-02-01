fcfa7d8d9aacc6980357671c61227c3f97f2b8ba
jyothivsp <sai.gurubilli@capgemini.com>
2017-02-01 16:28:49 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+        default = "ap-northeast-1"
+}
+variable "availability_zone" {
+        default = "ap-northeast-1a"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "aws_ami" {
+        default = "ami-5dd8b73a"
+}
+
+variable "key_name" {
+        default = "AWS_Tokyo"
+}
+
+variable "aws_access_key" {
+        default = "AKIAIMQKOPESL7JMLW5A"
+}
+
+variable "aws_secret_key" {
+        default = "DNx5GKLLSQJLwxfxTHUeKpAqWNWjvh36q2NURctI"
+}
